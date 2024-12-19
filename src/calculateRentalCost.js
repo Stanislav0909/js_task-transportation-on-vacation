02ce4ecabccc11c4d6d46b30a2b1b70fc861ea1d
@foxstudent106196 <mr.savchek.stanislav@gmail.com>
2024-12-19 19:08:23 +0100

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,21 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
+// eslint-disable-next-line no-unused-vars
 function calculateRentalCost(days) {
-  // write code here
-}
+  const dailyRate = 40;
+  const LOND_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERN_DISCOUNT = 20;
 
+  const basePrice = days * dailyRate;
+
+  if (days >= LOND_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERN_DISCOUNT;
+  }
+
+  return basePrice;
+}
 module.exports = calculateRentalCost;
